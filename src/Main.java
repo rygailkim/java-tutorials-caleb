5ecc20ea073fd5418c8008b6fb5b369ccb9d5b6e
unknown <rygailkim@gmail.com>
2024-09-23 13:59:49 +0800

    list and reading files

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter size: ");
-        int size = in.nextInt();
+        Scanner in = new Scanner(new File("src/test.txt"));
 
-        int[] grades = new int[size];
+        List<String> students = new ArrayList<>();
 
-        System.out.println("Enter " + size + " numbers:");
-        for(int i = 0; i < size; i++) {
-            grades[i] = in.nextInt();
+        while(in.hasNextLine()) {
+            students.add(in.nextLine());
+        }
+
+        for(int i = 0; i < students.size(); i++) {
+            System.out.println("Name " + (i + 1) + ": " + students.get(i));
         }
 
         in.close();
 
-        for(int i = 0; i < size; i++) {
-            System.out.println(grades[i]);
-        }
     }
 }
