abb129d98c3a47e4c7e4b613d8ce757d076222ef
unknown <rygailkim@gmail.com>
2024-09-24 09:42:06 +0800

    multi-dimensional array

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,22 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList<String> names = new LinkedList<String>();
-        names.push("Caleb");
-        names.push("Sue");
-        names.push("Sally");
+        int[][] data = {
+                {5, 10, 13, 14},
+                {16, 20, 22, 24},
+                {25, 29, 30, 35}
+        };
 
-        Iterator<String> it = names.iterator();
+        System.out.println(data[1][2]);
 
-        while(it.hasNext()) {
-            System.out.println(it.next());
-        }
+        data[1][2] = 49;
+        System.out.println(data[1][2]);
 
-        for (String s: names) {
-            System.out.println(s);
-        }
+        for(int i = 0; i < data.length; i++) {
+            for(int j = 0; j < data[i].length; j++) {
+                System.out.print(data[i][j] + " ");
+            }
+            System.out.println();
+         }
     }
 }
